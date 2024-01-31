af60960acddc0519fa18c84fb0f90ff60b0d581a
KERVY MANIEGO <kervymaniego@gmail.com>
2024-01-31 20:55:42 +0800

    isograms on different file

---
+++ isograms.rb
@@ -0,0 +1,40 @@
+# Isograms
+
+# ---my first attempt---#
+
+# What's wrong with this is that i'm  only comparing [0] to [1..-1]. I need to increment [0] also that that every element will be compared to each other.
+
+def is_isogram(x)
+  y = x.split("")
+  letter = y[0]
+
+  y[1..-1].each do |z|
+    if letter == z 
+      return false
+    end
+  end
+  true
+end
+
+puts is_isogram("kerk")
+
+
+#-----incremented letter----#
+
+def is_isogram(x)
+  y = x.split("")
+  i = 0
+  
+  while i < y.length - 1
+    letter = y[i]
+
+    y[i + 1..-1].each do |z|                                    # explanation [a, b, a, d, e]
+      return false if letter == z
+    end
+
+    i += 1
+  end
+  true
+end
+
+puts is_isogram("kerk")
