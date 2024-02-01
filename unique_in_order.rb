d884a32b42b457b2f59939bc106f258c3bd6a4d3
KERVY MANIEGO <kervymaniego@gmail.com>
2024-02-01 19:56:25 +0800

    final push for today's activity

--- unique_in_order.rb
+++ unique_in_order.rb
@@ -12,3 +12,15 @@ end
 unique_in_order('AABBCcDDAAABBEE')
 unique_in_order([1,2,2,3,3])
 
+
+
+puts "Big Wins: << = append, each_with_object = each_to_an_object"
+#attempt 
+
+
+# def arr_object(obj)
+#   sample = obj.each_with_object([]) { |each_element, lalagyanan| lalagyanan.push(each_element) if each_element > 51}
+#   puts sample.inspect
+# end
+
+# arr_object([14,13,52,58,60,45,99,90])
