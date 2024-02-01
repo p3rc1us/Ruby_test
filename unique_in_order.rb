5214895d3ad58385dd9c8e19f687a9a1a0091772
KERVY MANIEGO <kervymaniego@gmail.com>
2024-02-01 19:46:32 +0800

    modified is_a array

--- unique_in_order.rb
+++ unique_in_order.rb
@@ -4,12 +4,11 @@ def unique_in_order(arr)
         x = y.each_with_object([]) { |element, result| result << element unless result.last == element }
         puts x.inspect
     elsif arr.is_a? Array
-        z = arr.to_a
-        u = arr.uniq
-        puts u.inspect
+        z = arr.uniq
+        puts z.inspect
     end
 end
 
 unique_in_order('AABBCcDDAAABBEE')
-unique_in_order([1,1,2,2,3,1,3])
+unique_in_order([1,2,2,3,3])
 
