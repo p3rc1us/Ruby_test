87a4c0f9f819743f775c734a9ee1bd7612b4e175
KERVY MANIEGO <kervymaniego@gmail.com>
2024-02-03 13:25:08 +0800

    refactored to one liner

--- disemvowel_trolls.rb
+++ disemvowel_trolls.rb
@@ -1,13 +1,23 @@
-# def disemvowel(x)
-#     x = x.split('').include?("e") if x.instance_of? String;
-#     true
-# end
-
-
+#first attempt
 
 def disemvowel(x)
-y = x.split('')
-y.each_with_object([]) { |letter, lagayan| lagayan << letter if !["a", "e", "i", "o", "u"].include?(letter.downcase) }.join
+    (x.split('')).each_with_object([]) { |letter, lagayan| lagayan << letter if !["a", "e", "i", "o", "u"].include?(letter.downcase)} .join 
 end
 
+
+# def disemvowel(x)
+# y = x.split('')
+# y.each_with_object([]) { |letter, lagayan| lagayan << letter if !["a", "e", "i", "o", "u"].include?(letter.downcase) }.join
+# end                     #letter != ["a", "e", "i", "o", "u"]
+
 puts disemvowel("Ooy labas ka na jan!")
+
+
+
+
+
+# def disemvowel(x)
+#     y = x.split('')
+#     y.each_with_object([]) { |element, container| container << element if x == "a","e","i","o","u" }
+# end
+
