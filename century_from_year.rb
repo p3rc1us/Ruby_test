58926daf58b5398fc9f10709f40fd157b31f681f
KERVY MANIEGO <kervymaniego@gmail.com>
2024-02-07 10:02:51 +0800

    refactored identify_century to one line

--- century_from_year.rb
+++ century_from_year.rb
@@ -1,12 +1,21 @@
+# refactored
 def identify_century(x)
-   y = x / 100
-
-   if x % 100 == 0
-    puts y 
-   else
-    puts y+1
-   end
-    
+   puts x % 100 == 0 ? (x / 100) : (x / 100) + 1
 end
 
-identify_century(2001)
+identify_century(1601)
+
+# from:
+
+# def identify_century(x)
+#    y = x / 100
+
+#    if x % 100 == 0
+#     puts y 
+#    else
+#     puts y+1
+#    end
+    
+# end
+
+# identify_century(2001)
