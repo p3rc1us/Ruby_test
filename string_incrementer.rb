6be25f6ae5a9b7af19c3e5dccee651491edb8795
KERVY MANIEGO <kervymaniego@gmail.com>
2024-02-21 14:35:15 +0800

    string incrementer done

--- string_incrementer.rb
+++ string_incrementer.rb
@@ -1,11 +1,16 @@
 def string_incrementer(str)
+
+# know the condition
   numbers = (str.scan(/\d+/).join.to_i + 1).to_s
 
-  words = str.scan(/\D+/).join
-  puts words << numbers
+  if numbers then
+    words = str.scan(/\D+/).join
+    puts words << numbers
+  else words = (str.scan(/\D+/).join) << 1
+  end
 
 end
 
-puts string_incrementer("hello13")
+puts string_incrementer("hello")
 
 #line 3-naextract mo na ung INT, extract mo ung naiwan na word para i join sa na-increment na INT
