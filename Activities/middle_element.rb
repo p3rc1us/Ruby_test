4a0b29b7365dd2e1d15993fe51eac9473a40989a
KERVY MANIEGO <kervymaniego@gmail.com>
2024-03-21 21:15:14 +0800

    SA WAKAS HAHA min and max only bcos understood i only have 3 elements

--- Activities/middle_element.rb
+++ Activities/middle_element.rb
@@ -1,10 +1,28 @@
-def smallest(x)
-  y = x.min
-    x.each_with_index do | e, i|
-      if y == e
-        puts i
-      end
+# def smallest(x)
+#   y = x.min
+#     x.each_with_index do | e, i|
+#       if y == e
+#         puts i
+#       end
+#     end
+# end
+
+# smallest([2, 3, 1])
+#         # 0, 1, 2
+
+  # arr.each_with_index do | e, i |
+  #   if e > arr[i+1] && e < arr[i+2]
+  #     e
+  #   end
+
+  # end
+
+def smallest(arr)
+  arr.each_with_index do | e, i |
+    if e != arr.max && e != arr.min
+      puts i
     end
+  end
 end
 
-smallest([2,3,1])
+smallest([590, 36, 45])
