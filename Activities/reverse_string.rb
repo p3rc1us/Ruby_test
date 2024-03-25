418fdf637bca69d8f0cd8f5abfd1ba30aa91c289
KERVY MANIEGO <kervymaniego@gmail.com>
2024-03-25 20:56:32 +0800

    cleaning

--- Activities/reverse_string.rb
+++ Activities/reverse_string.rb
@@ -1,7 +1,6 @@
 def reverse(str)
   x = str.split("")
-  puts x.each_with_object([]) { | element, box | box.push(x.pop) until x.empty? }.join
+  x.each_with_object([]) { | element, box | box.push(x.pop) until x.empty? }.join
 end
 
-
-reverse("hello")
+puts reverse("kervy")
