e94ae7e704ca95450331a4ebe07e88bd84a9a422
KERVY MANIEGO <kervymaniego@gmail.com>
2024-04-26 12:05:30 +0800

    added chomp

--- Activities/perfect_number.rb
+++ Activities/perfect_number.rb
@@ -1,19 +1,28 @@
-def perfect(int)
-  arr = (1..int).to_a
+# def perfect(int)
+#   arr = (1..int).to_a
 
-  final = arr.each_with_object([]) do |e, c|
-    if int % e == 0 && e != int
-      c << e
+#   final = arr.each_with_object([]) do |e, c|
+#     if int % e == 0 && e != int
+#       c << e
+#     end
+#   end
+
+#   if final.sum == int
+#     return final.inspect
+#   else "not a perfect number"
+#   end
+# end
+
+# puts perfect(8128)
+
+def input
+  loop do
+    puts "whats your age?"
+    age = gets.chomp().to_i
+    if age == 18
+      return
     end
   end
-
-  if final.sum == int
-    return final.inspect
-  else "not a perfect number"
-  end
-
-
-
 end
 
-puts perfect(8128)
+input
