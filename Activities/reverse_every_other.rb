79bf0d209abb572731493698b3dbc0e591cc5bc0
KERVY MANIEGO <kervymaniego@gmail.com>
2024-05-03 20:44:36 +0800

    exercise: reverse every other

---
+++ Activities/reverse_every_other.rb
@@ -0,0 +1,12 @@
+def every_other(str)
+  str.split(/\s+/).map.with_index do | string, index |
+    if index.odd?
+      x = string.split("")
+      x.each_with_object([]) { | element, box | box.push(x.pop) until x.empty? }.join
+    else
+      string
+    end
+  end
+end
+
+puts every_other("did it work, hatdog?")
