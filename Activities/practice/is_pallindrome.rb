666143f7eca957b3ac25d95b5f92a97195bf3e10
KERVY MANIEGO <kervymaniego@gmail.com>
2024-05-13 17:23:03 +0800

    attempt

--- Activities/practice/is_pallindrome.rb
+++ Activities/practice/is_pallindrome.rb
@@ -6,3 +6,13 @@ def is_palindrome(x)
 end
 
 puts is_palindrome(121)
+
+# attempting not to convert it to a string
+# def is_palindrome(x)
+#   y = []
+#   y = y.push(x)
+#   y.each_char do |z|
+#     z.to_i
+#   end
+#   y.each_with_object([]) { | e, c | c << e }
+# end
