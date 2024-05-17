039716392c509ba834493b484b2bdd23dad5b4c5
KERVY MANIEGO <kervymaniego@gmail.com>
2024-05-17 14:16:01 +0800

    attempt good

--- Activities/practice/is_pallindrome.rb
+++ Activities/practice/is_pallindrome.rb
@@ -1,18 +1,23 @@
+# def is_palindrome(x)
+#   if x.to_s.reverse == x.to_s
+#     return true
+#   else false
+#   end
+# end
+
+# puts is_palindrome(122)
+
+# attempting not use string
 def is_palindrome(x)
-  if x.to_s.reverse == x.to_s
-    return true
-  else false
+  y = 0
+
+  while x > 0 do
+    y = y*10
+    y = y + (x%10)
+    x = x/10
+
   end
+  y
 end
 
-puts is_palindrome(121)
-
-# attempting not to convert it to a string
-# def is_palindrome(x)
-#   y = []
-#   y = y.push(x)
-#   y.each_char do |z|
-#     z.to_i
-#   end
-#   y.each_with_object([]) { | e, c | c << e }
-# end
+puts is_palindrome(123)
