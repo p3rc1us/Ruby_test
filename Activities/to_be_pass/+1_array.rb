c6001fb24523671f53bf38ad371aa16583fec137
KERVY MANIEGO <kervymaniego@gmail.com>
2024-06-16 23:40:43 +0800

    each doesnt return a new array, map does

--- Activities/to_be_pass/+1_array.rb
+++ Activities/to_be_pass/+1_array.rb
@@ -1,7 +1,13 @@
 def plus_one(arr)
   n = (arr.join.to_i) + 1
-  (n.to_s).split("")
+  m = n.to_s.split("")
+  y = []
+
+  m.each do |x|
+    y << x.to_i
+  end
+  return y
 end
 
-puts plus_one([1,2,3]) # .inspect
+puts plus_one([1,2,3])
 puts plus_one([1,2,9])
