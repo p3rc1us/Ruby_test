58c50a1a1b464608334b4915a1d8f363b995790b
KERVY MANIEGO <kervymaniego@gmail.com>
2024-06-23 22:53:17 +0800

    <=> operator doesnt need explicit increment

--- Activities/to_be_pass/new.rb
+++ Activities/to_be_pass/new.rb
@@ -1,12 +1,6 @@
 def sorting(arr)
-
-  hash_final = arr.each_with_object({}) do | k, hash_container |
-    hash_container[k] = k.length
-  end
-
+  arr.sort {|a, b| a.length <=> b.length }
+  # arr.sort_by { |x| x.length }
 end
 
 puts sorting(["abcde", "abdf", "adeab", "abdgeee", "bdefa", "abc", "ab", "a", "bacdef"])
-
-
-  # arr.sort_by { |x| x.length }
