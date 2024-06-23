92c6499ed9e58ef698e0a6d598fd5389b37b86de
KERVY MANIEGO <kervymaniego@gmail.com>
2024-06-23 22:40:46 +0800

    converted to hash with respective length

--- Activities/to_be_pass/new.rb
+++ Activities/to_be_pass/new.rb
@@ -1,5 +1,12 @@
 def sorting(arr)
-  arr.sort_by { |x| x.length }
+
+  hash_final = arr.each_with_object({}) do | k, hash_container |
+    hash_container[k] = k.length
+  end
+
 end
 
 puts sorting(["abcde", "abdf", "adeab", "abdgeee", "bdefa", "abc", "ab", "a", "bacdef"])
+
+
+  # arr.sort_by { |x| x.length }
