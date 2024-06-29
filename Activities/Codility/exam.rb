dc1a45cdb2a950e5ef57990e31200f09f23874b4
KERVY MANIEGO <kervymaniego@gmail.com>
2024-06-29 21:54:06 +0800

    modified

--- Activities/Codility/exam.rb
+++ Activities/Codility/exam.rb
@@ -1,19 +1,33 @@
-def arrow(n)
+# def arrow(n)
+#   box = []
+
+
+#   n.each_char do |x|
+#     if x =~ /^['<>^v']+$/
+#       box << n.count(x)
+#     else
+#       nil
+#     end
+#   end
+#   # max = box.max
+#   # size = n.length
+#   # size - max
+#   n.length - box.max
+# end
+
+def arrow(s)
   box = []
 
+  return nil if s !~ /^[<>^v]+$/
 
-  n.each_char do |x|
-    if x =~ /^['<>^v']+$/
-      box << n.count(x)
-    else
-      nil
-    end
+  s.each_char do |x|
+    box << s.count(x)
   end
-  # max = box.max
-  # size = n.length
-  # size - max
-  n.length - box.max
+  s.size - box.max
 end
+
+puts arrow("<>^^^").inspect
+
 
 
 puts arrow("<v<^^^^^^^^^^^<<^")
