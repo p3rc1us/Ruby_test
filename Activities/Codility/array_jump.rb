709bf02e764463d737a20147f56eda3d81a2829b
KERVY MANIEGO <kervymaniego@gmail.com>
2024-07-01 12:23:24 +0800

    pawn exercise without edge case

---
+++ Activities/Codility/array_jump.rb
@@ -0,0 +1,14 @@
+def out_of_bounce(arr)
+  jump_count = 0
+  new_index = arr.index(arr[0]) + arr[0]
+
+  while new_index <= arr.size do
+    new_index = arr.index(arr[new_index]) + arr[new_index]
+    jump_count += 1
+  end
+
+  jump_count
+end
+
+puts out_of_bounce([1,0,3,4,5])
+                #   0,1,2,3,4
