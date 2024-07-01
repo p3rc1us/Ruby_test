98d0fa7da457e185f0f467c99281d700aa11b1df
KERVY MANIEGO <kervymaniego@gmail.com>
2024-07-01 15:16:34 +0800

    done exercise: no need to use .index bcos i already have the index

--- Activities/Codility/array_jump.rb
+++ Activities/Codility/array_jump.rb
@@ -1,17 +1,17 @@
 def out_of_bounce(arr)
   jump_count = 0
-  new_index = arr.index(arr[0]) + arr[0]
+  new_index = 0
 
-  while new_index <= arr.size do
-    new_index = arr.index(arr[new_index]) + arr[new_index]
-
-    return -1 if new_index == arr.index(arr[0]) + arr[0]
-
+  while new_index <= arr.size && new_index >= 0 do
     jump_count += 1
+    next_index = new_index + arr[new_index]
+                  #this is alread my index + now the value of it
+    return -1 if next_index == new_index
+    new_index = next_index
   end
 
   jump_count
 end
 
-puts out_of_bounce([1,2,3,4,5])
-                #   0,1,2,3,4
+puts out_of_bounce([1,2,3,4])
+                #   0,1,2,3
