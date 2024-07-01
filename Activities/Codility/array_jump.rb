2358ec4fdb9dd4f08b87742f68df3b9697af23cb
KERVY MANIEGO <kervymaniego@gmail.com>
2024-07-01 12:34:49 +0800

    return -1 if infinite loop

--- Activities/Codility/array_jump.rb
+++ Activities/Codility/array_jump.rb
@@ -4,6 +4,9 @@ def out_of_bounce(arr)
 
   while new_index <= arr.size do
     new_index = arr.index(arr[new_index]) + arr[new_index]
+
+    return -1 if new_index == arr.index(arr[0]) + arr[0]
+
     jump_count += 1
   end
 
